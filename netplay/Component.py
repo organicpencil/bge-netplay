463feac5f64587ef5e08581f7429d02e2e1c5006
Lee <pqftgs@gmail.com>
2015-03-03 23:55:04 -0600

    Fixed inconsistency with next component ID

--- netplay/Component.py
+++ netplay/Component.py
@@ -231,7 +231,7 @@ class ClientComponentSystem(ServerComponentSystem):
         self.active_components_[net_id] = comp
 
         if net_id > self.next_active_id_:
-            self.next_active_id_ = net_id
+            self.next_active_id_ = net_id + 1
 
         return comp
 
