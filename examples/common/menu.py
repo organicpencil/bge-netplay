0d72c4ab72f6a1f724212bc639ea1e02d31ed6bf
Lee Pugh <pqftgs@gmail.com>
2017-04-19 03:31:47 -0500

    Remove compz dependency from menu

--- examples/common/menu.py
+++ examples/common/menu.py
@@ -1,138 +1,51 @@
 import bge
-import compz
 from netplay import host
 
 
+class Button:
+    def __init__(self, owner):
+        self.owner = owner
+        owner['button'] = self
+
+    def on_click(self):
+        pass
+
+
 class Menu:
-    def __init__(self, cont):
-        self.cont = cont
+    def __init__(self):
+        objects = bge.logic.getCurrentScene().objects
 
-        skin = bge.logic.expandPath('//../common/skin.png')
-        font = bge.logic.expandPath('//../common/Anonymous Pro.ttf')
-        style = compz.Style(skin, font)
-        icons = bge.logic.expandPath('//../common/icons/')
+        button = Button(objects['server'])
+        button.on_click = self.start_server
 
-        self.c = c = compz.Compz(style)
+        button = Button(objects['client'])
+        button.on_click = self.start_client
 
-        panel = compz.Panel()
-        panel.position = [50, 50]
-        panel.width = 100
-        panel.height = 94
-        c.addComp(panel)
+    def update(self):
+        cam = bge.logic.getCurrentScene().active_camera
+        mpos = bge.logic.mouse.position
+        mvec = cam.getScreenVect(*mpos)
+        mvec.negate()
+        mvec += cam.worldPosition
+        ob, pos, normal = cam.rayCast(mvec, cam, 200.0, 'btn', 1, 1)
+        if ob is not None:
+            if bge.logic.mouse.events[bge.events.LEFTMOUSE] == bge.logic.KX_INPUT_ACTIVE:
+                ob['button'].on_click()
 
-        server = compz.Button("Server")
-        server.events.set(compz.EV_MOUSE_CLICK, self.start_server)
-        server.icon = compz.Icon(icons + 'network-server.png')
-
-        client = compz.Button("Client")
-        client.events.set(compz.EV_MOUSE_CLICK, self.start_client)
-        client.icon = compz.Icon(icons + 'computer.png')
-
-        end = compz.Button("Exit")
-        end.events.set(compz.EV_MOUSE_CLICK, self.end_game)
-        end.icon = compz.Icon(icons + 'application-exit.png')
-
-        panel.addComp(server)
-        panel.addComp(client)
-        panel.addComp(end)
-
-    def start_server(self, sender):
+    def start_server(self):
+        print ("SERVER")
         bge.logic.netplay = host.ServerHost()
         bge.logic.getCurrentScene().replace('Scene')
 
-    def start_client(self, sender):
+    def start_client(self):
         bge.logic.netplay = host.ClientHost()
         bge.logic.getCurrentScene().replace('Scene')
-
-    def end_game(self, sender):
-        bge.logic.endGame()
-
-    def update(self):
-        self.c.update()
 
 
 def main(cont):
     owner = cont.owner
     m = owner.get('menu', None)
     if m is None:
-        owner['menu'] = Menu(cont)
+        owner['menu'] = Menu()
     else:
         m.update()
-
-"""
-from bge import logic as g
-import compz
-cont = g.getCurrentController()
-o = cont.owner
-
-if "init" not in o:
-    o["C"] = compz.Compz()
-
-    btnStyle = compz.Style(name="button", stylesPath=g.expandPath("//default/"))
-    panelStyle = compz.Style(name="panel", stylesPath=g.expandPath("//default/"))
-    entryStyle = compz.Style(name="entry", stylesPath=g.expandPath("//default/"))
-
-    pan = compz.Panel(panelStyle)
-    pan.position = [20, 20]
-    pan.width = 120
-    pan.height = 200
-    o["C"].addComp(pan)
-
-    text = compz.Entry(style=entryStyle)
-
-    passwd = compz.Entry(style=entryStyle)
-    passwd.text = "MyPassword"
-    passwd.masked = True
-
-    def click(sender):
-        print("Clicked on button " + sender.text)
-        text.text = sender.text
-
-    play = compz.Button("Play", btnStyle)
-    play.events.set(compz.EV_MOUSE_CLICK, click)
-    play.icon = compz.Icon(g.expandPath("//control_play.png"))
-
-    options = compz.Button("Options", btnStyle)
-    options.events.set(compz.EV_MOUSE_CLICK, click)
-    options.icon = compz.Icon(g.expandPath("//options.png"))
-
-    exit = compz.Button("Exit", btnStyle)
-    exit.events.set(compz.EV_MOUSE_CLICK, click)
-    exit.icon = compz.Icon(g.expandPath("//door_out.png"))
-
-    pan.addComp(play)
-    pan.addComp(options)
-    pan.addComp(exit)
-    pan.addComp(text)
-    pan.addComp(passwd)
-    pan.addComp(compz.Label("Hello World!"))
-
-    gpan = compz.Panel(panelStyle)
-    gpan.position = [10, 10]
-    gpan.width = 130
-    gpan.height = 200
-    gpan.layout = compz.GridLayout()
-    o["C"].addComp(gpan)
-
-    gpan.centerOnScreen()
-
-    lbl = gpan.addComp(compz.Label("Click:"))
-    lbl.textAlignment = compz.TEXT_ALIGN_RIGHT
-
-    btn = gpan.addComp(compz.Button("Here", btnStyle))
-    btn.column = 1
-    btn.margin = [2, 6]
-
-    lbl = gpan.addComp(compz.Label("And also:"))
-    lbl.row = 1
-    lbl.textAlignment = compz.TEXT_ALIGN_RIGHT
-
-    btn = gpan.addComp(compz.Button("Here", btnStyle))
-    btn.row = 1
-    btn.column = 1
-    btn.margin = [2, 6]
-
-    o["init"] = 1
-else:
-    o["C"].update()
-"""
--- setup_linux64.sh
+++ setup_linux64.sh
@@ -3,10 +3,3 @@ ln -s netplay examples/common/netplay
 cd netplay/enet/
 
 wget http://pqftgs.net/downloads/netplay/enet.cpython-35m-x86_64-linux-gnu.so
-
-cd ../../examples/common
-
-wget http://pqftgs.net/downloads/netplay/compz.zip
-rm -rf compz
-unzip compz.zip
-rm compz.zip
